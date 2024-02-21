dc8171563b940232529b1cc7e88515a19fc3248a
Jun Santos <xjunsantos@gmail.com>
2024-02-21 10:59:25 +0800

    Upload files

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw  \Illuminate\Validation\ValidationException::withMessages([
+                'email' => ['The credentials you entered is incorrect.']
+            ]);
+        }
+    }
+}
