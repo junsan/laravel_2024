8c2597d9f55e02792253a75a79ada51e4d681479
Jun Santos <xjunsantos@gmail.com>
2024-02-24 15:30:50 +0800

    Token Auth

---
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw  \Illuminate\Validation\ValidationException::withMessages([
+                'email' => ['The credentials you entered is incorrect.']
+            ]);
+        }
+
+        return response()->json([
+            'user' => $user,
+            'token' => $user->createToken('laravel_api_token')->plainTextToken
+        ]);
+    }
+}
